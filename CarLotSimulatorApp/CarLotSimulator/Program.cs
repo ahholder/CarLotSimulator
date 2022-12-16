16e79ba6ea169cb579de868ac7066d00871c4712
Alexander Holder <drowish95z@gmail.com>
2022-12-16 11:26:06 -0600

    Main and Bonus

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -18,6 +18,51 @@ namespace CarLotSimulator
             //Set the properties for each of the cars
             //Call each of the methods for each car
 
+            var lot = new CarLot();
+
+            var ford = new Car();
+            ford.Year = 2010;
+            ford.Make = "Ford";
+            ford.Model = "Focus";
+            ford.IsDriveable = false;
+            ford.EngineNoise = "Pffft";
+            ford.HonkNoise = "Beep";
+            lot.Stock.Add(ford);
+
+            var chevy = new Car();
+            chevy.Year = 2001;
+            chevy.Make = "Chevy";
+            chevy.Model = "Equinox";
+            chevy.IsDriveable = true;
+            chevy.EngineNoise = "Vroom-Vroom";
+            chevy.HonkNoise = "Beep-Beep";
+            lot.Stock.Add(chevy);
+
+            var chevy2 = new Car();
+            chevy2.Year = 2020;
+            chevy2.Make = "Chevy";
+            chevy2.Model = "Malibu";
+            chevy2.IsDriveable = true;
+            chevy2.EngineNoise = "Vrmmm";
+            chevy2.HonkNoise = "Beep";
+            lot.Stock.Add(chevy2);
+
+
+            foreach (var vehicle in lot.Stock)
+            {
+                vehicle.MakeEngineNoise(vehicle.EngineNoise);
+                vehicle.MakeHonkNoise(vehicle.HonkNoise);
+            }
+            Console.WriteLine("---------------");
+            Console.WriteLine("BONUS");
+            Console.WriteLine("---------------");
+            foreach (var vehicle in lot.Stock)
+            {
+                Console.WriteLine("Make: " + vehicle.Make);
+                Console.WriteLine("Year: " + vehicle.Year);
+                Console.WriteLine("Model: " + vehicle.Model);
+            }
+
             //*************BONUS*************//
 
             // Set the properties utilizing the 3 different ways we learned about, one way for each car
